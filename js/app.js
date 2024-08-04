943f8b5aa7fa8df45ec3a3fc8e24419b301f0701
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-08-04 19:59:23 -0400

    Add html elements to display the game instructions, current state of the game, number of attempts, grid of squares to display the user guess, and a new game button. Alsoadd basic styling to properly display all the elements

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+
+// PSEUDOCODE
+
+// // define a variable to set maximum attempts to 5-7
+// // define a variable to access grid elements
+// // define a variable called currentCell to store and modify clicked cell
+// // define a variable to store target word/words
+// // create an onscreen keyboard
+
+
+// // create a render function
+// // create an init function
+// // create a startNewGameFunction
+// // create a checkForMatch function to check the user guess against target word
+// // create handleClick function
+// // create updateBoard function to update UI on the page
+// // create updateMessage function to message win/fail
