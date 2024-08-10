255d3158263c3b736d261fc58a326c77abab0c19
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-08-10 02:50:40 -0400

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# wordle-browser-game
+# <p align="center">WORDLE</p>
+![alt text](image.png)
+
+Wordle is a popular online word puzzle game where players have five attempts to guess a five-letter word. After each guess, the game provides feedback by highlighting letters in three possible colors:
+
+Green: The letter is in the correct position.  
+Yellow: The letter is in the word but in the wrong position.  
+Gray: The letter is not in the word.  
+
+Players use these clues to refine their guesses and ultimately find the correct word. Wordle became widely popular for its simplicity and daily challenge format, where a new word is available to guess each day.
+# Getting started:
+1. As a user, I want to see clear instructions on how to play the Wordle game when the page loads.  
+1. As a user, I want to be able to start the game by clicking 'New Game'.  
+1. As a user, I want to see a grid of cells that can be clicked to guess the word.  
+2. As a user, I want to be able to use my keyboard or an on-screen keyboard so that I can type my guess.  
+1. As a user, I want to see letters that are in the correct place highlighted in green.  
+1. As a user, I want to see letters that are misplaced but still exist in the target word highlighted in yellow.    
+1. As a user, I want to see letters that do not exist in the target word highlighted in gray.  
+1. As a user, I want to see a message when I correctly guess the target word within the first 5-7 attempts.  
+
+
+```js 
+PSEUDOCODE
+
+// define a variable to set maximum attempts to 5-7
+// define a variable to access grid elements
+// define a variable called currentCell to store and modify clicked cell
+// define a variable to store target word/words
+// create an onscreen keyboard
+
+
+// create a render function
+// create an init function
+// create a startNewGameFunction
+// create a checkForMatch function to check the user guess against target word
+// create handleClick function
+// create updateBoard function to update UI on the page
+// create updateMessage function to message win/fail
+```
+
+
+
+# Acknowledgments
+- Special thanks to **[@chamer079](https://github.com/chamer079)** - for their support and experience.
+- **ChatGPT** 
+
+# Technologies used:
+
+HTML, CSS,  JavaScript
+
+# Next steps: 
+
+1. Add New Game button 
+2. Incorporate mobile-friendly layout
+3. Add dark/light mode functionality
+4. Expand target word base
+5. Add multiplayer functionality
+
--- js/app.js
+++ js/app.js
@@ -1,142 +1,100 @@
-// CONSTANTS //
 const attemptEl = document.querySelector('#attempts')
 const instructionsEl = document.querySelector('#instructions')
 const messageEl = document.querySelector('#message')
 const squareEls = document.querySelectorAll('.sqr')
 const rowEls = document.querySelectorAll('.row')
-// BUTTON ELEMENTS
 const submitButtonEl = document.querySelector('#submit')
 const newGameButtonEl = document.querySelector('#new-game')
-
 const userGuesses = [[], [], [], [], []]
 const targetWords = [ 'agree', 'whale', 'clear', 'clean', 'great', 'small'];
 const randomWord = targetWords[Math.floor(Math.random() * targetWords.length)];
-
-
-
-// VARIABLES //
 let attempts = 5
-let currentRowIndex = 0 // identifies which row of 5 user is typing at
-let nextRowStartIndex = currentRowIndex * 5 // identifies the starting index at the next row
-
-
-// CACHED ELEMENT REFERENCES
-
-// Add handleSubmit function to submit button to check if user guess matches the target word 
-submitButtonEl.addEventListener('click', handleSubmit)
-
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    squareEls.forEach((squareEl) => {
-        // Focus first cell when DOM loads
-        squareEls[0].focus()
-        // Add a keydown function to every square to retreive the pressed key value and display it on the browser
-        squareEl.addEventListener('keydown', handleKeydown)
-
-    });
-})
+let currentRowIndex = 0 
+let nextRowStartIndex = currentRowIndex * 5
 
 document.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
         handleSubmit()
     }
 })
+submitButtonEl.addEventListener('click', handleSubmit)
+
+squareEls.forEach((squareEl) => {
+    squareEls[0].focus()
+    squareEl.addEventListener('keydown', handleKeydown)
+});
+
 
 function handleKeydown(event) { 
     const squareEl = event.target;
     const squareIndex = Number(squareEl.id);
-    // If the current square does not have a value and pressed key is one from the actual Alphabet, execute this code
     if (squareEl.textContent.length == 0 && /^[a-zA-Z]$/.test(event.key)) {
-        // Display the pressed key value in squareEL
         squareEl.textContent = event.key
-        // Move focus to the next cell/sqr after current one gets a value
-        squareEls[squareIndex+1]?.focus() // ChatGpt help here with the ? mark
-            // as long as current row does NOT have 5 characters...
-            // ..then push the pressed key value into userGuesses ...
-            // ..array at that row's index
+        squareEls[squareIndex+1]?.focus() 
             if (userGuesses[currentRowIndex].length < 5) {
                 userGuesses[currentRowIndex].push(event.key)
                 callForSubmit()
             }
-            // Once current row has 5 chars push most recently clicked key values into the next nested array
             else {
                 currentRowIndex++
                 userGuesses[currentRowIndex].push(event.key)
                 callForSubmit()
             }
     }
-    // if current square have a value and user clicks 'backspace' then remove its value
     else if (squareEl.textContent.length == 1 && event.key == 'Backspace') {
         squareEl.textContent = ''
     }
-    // if current square does NOT have a value and user clicks 'backspace' then remove value of previous square
     else if (squareEl.textContent.length == 0 && event.key == 'Backspace') {
-        // Move focus back to the previous cell and remove its content when backspace clicked
         squareEls[Number(event.target.id)-1].focus()
         squareEls[Number(event.target.id)-1].textContent = ''
     }
 }
 
-// FUNCTIONS
 function callForSubmit() {
-    // if user typed first guess/5 character disable other cells to force submit
     if (userGuesses[currentRowIndex].length === 5) {
         messageEl.textContent = "SUBMIT YOUR GUESS"
         messageEl.classList.add('ask-submit')
-        
-        // remove tabindex attribute from the next row in order to disable typing on the next row..
-        // .. so that user knows they need to submit their current guess
         squareEls.forEach(squareEl => {
             squareEl.removeAttribute('tabindex')
         })
     } 
 }
 
-
-
-
 function handleSubmit() {
-        
-    if (userGuesses[currentRowIndex].length === 5) {
+    if (userGuesses[currentRowIndex]?.length === 5) {
         handleColors()   
         checkForWin()
     }
-    
 }
 
 function checkForWin() {
-        // concat the user guess since it is in a nested array
         const guessedWord = userGuesses[currentRowIndex].join('')
-        // compare user input with the target word
         if (guessedWord === randomWord) {
             messageEl.textContent = "CONRATULATIONS! YOU GUESSED RIGHT!"
-        } else {
+        }
+        else {
             attempts--
             currentRowIndex++
             attemptEl.textContent = `Attempts: ${attempts}`;
             if (attempts > 0) {
                 nextAttempt();
-            }else {
+            }
+            else {
                 messageEl.textContent = "GAME OVER. The word was " + randomWord.toUpperCase();
             }
         }
     }
 
-function handleColors() {
-        // Check if the user guessed the character at the right index
+    function handleColors() {
         userGuesses[currentRowIndex].forEach((userGuessChar, index) => {
             const squareEl = squareEls[currentRowIndex * 5 + index]
-            // If the character user clicks is at the same index as the char from target word 
             if (userGuessChar == randomWord[index]) {
                 squareEl.style.backgroundColor = 'lightGreen'
             }
-            // If the character user clicks exists somewhere in the target word 
             else if (randomWord.includes(userGuessChar)) {
                 squareEl.style.backgroundColor = 'Yellow'
-            } else if (!randomWord.includes(userGuessChar)) {
+            }
+            else if (!randomWord.includes(userGuessChar)) {
                 squareEl.style.backgroundColor = 'gray'
             }
     })
@@ -146,12 +104,10 @@ function handleColors() {
 function nextAttempt() {
     nextRowStartIndex = currentRowIndex * 5
     messageEl.textContent = 'TRY AGAIN'
-    // attempts--
     squareEls.forEach((squareEl, index) => {
         if (index >= nextRowStartIndex && index < nextRowStartIndex + 5){
             squareEl.setAttribute('tabindex', 0)
         } 
     })
     squareEls[nextRowStartIndex].focus();
-
 }
